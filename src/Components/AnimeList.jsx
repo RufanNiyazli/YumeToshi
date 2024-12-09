8914ab49ca7fa00c57069932cfad881192549cde
Rufan Niyazli <developerx73@gmail.com>
2024-12-09 23:25:54 +0400

    car css overlay css

--- src/Components/AnimeList.jsx
+++ src/Components/AnimeList.jsx
@@ -1,28 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAnime } from "../Slices/AnimeListSlice";
+import { IoIosArrowDropup, IoIosArrowDropdown } from "react-icons/io";
+import "../Css/AnimeList.css";
+
 function AnimeList() {
   const dispatch = useDispatch();
   const { animes, loading } = useSelector((store) => store.animeList);
+  const [expandedCard, setExpandedCard] = useState(null);
 
   useEffect(() => {
     dispatch(getAnime());
   }, [dispatch]);
+
   if (loading) {
     console.log("loading...");
-  } else {
-    console.log(animes);
+    return <div>Yüklənir...</div>;
   }
 
+  const toggleOverlay = (id) => {
+    setExpandedCard(expandedCard === id ? null : id);
+  };
+
   return (
-    <div>
+    <div className="card-container">
       {animes &&
         animes.map((anime) => (
-          <div key={anime.mal_id}>
-            <h2>{anime.title}</h2>
-            <img src={anime.images.jpg.image_url} alt="" />
-            <p>{anime.episodes}</p>
-            <span>{anime.score}</span>
+          <div key={anime.mal_id} className="card">
+            <img src={anime.images.jpg.image_url} alt={anime.title} />
+            <div
+              className={`overlay ${
+                expandedCard === anime.mal_id ? "expanded" : ""
+              }`}
+            >
+              <div className="text-icon">
+                <h3>{anime.title}</h3>
+                <div
+                  className="icon"
+                  onClick={() => toggleOverlay(anime.mal_id)}
+                >
+                  {expandedCard === anime.mal_id ? (
+                    <IoIosArrowDropdown />
+                  ) : (
+                    <IoIosArrowDropup />
+                  )}
+                </div>
+              </div>
+              {expandedCard === anime.mal_id && (
+                <p>
+                  {anime.synopsis
+                    ? anime.synopsis.slice(0, 100) + "..."
+                    : "Təsvir yoxdur."}
+                </p>
+              )}
+            </div>
           </div>
         ))}
     </div>
